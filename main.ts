73667c78ce732b2f00bde98264957f14ef86af4b
LudumStudios <65925982+Shiven121S@users.noreply.github.com>
2021-01-01 18:42:32 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -16,11 +16,16 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Crate, function (sprite, otherSp
         otherSprite.destroy(effects.disintegrate, 500)
     })
     timer.after(50, function () {
-        Reward = randint(1, 2)
+        Reward = randint(1, 3)
         if (Reward == 1) {
             info.changeScoreBy(20)
         } else if (Reward == 2) {
             Ammo += 5
+            if (Ammo > 20) {
+                Ammo = 20
+            }
+        } else if (Reward == 3) {
+            statusbar.value += 5
         }
     })
 })
@@ -279,6 +284,30 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
+function Crate_maker () {
+    if (Math.percentChance(50)) {
+        Reward_Crate = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . e f f f f f f e . . . . 
+            . . . . e f f e e f f e . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . e f f e e f f e . . . . 
+            . . . . e f f f f f f e . . . . 
+            . . . . e f f f f f f e . . . . 
+            . . . . e e e e e e e e . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Crate)
+        tiles.placeOnTile(Reward_Crate, tiles.getTileLocation(randint(0, 44), 0))
+        Reward_Crate.setVelocity(0, 90)
+    }
+}
 sprites.onOverlap(SpriteKind.Crate, SpriteKind.Crate, function (sprite, otherSprite) {
     tiles.placeOnTile(sprite, tiles.getTileLocation(randint(0, 44), 0))
     sprite.setVelocity(0, 90)
@@ -329,6 +358,42 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . 5 5 5 5 5 5 5 5 5 5 . . . 
         `)
 })
+function Enemy_Maker () {
+    Basic_Enemy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 f f f f f f f f 2 . . . 
+        . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+        `, SpriteKind.BasicEnemy)
+    tiles.placeOnTile(Basic_Enemy, tiles.getTileLocation(randint(0, 44), 0))
+    Basic_Enemy.setVelocity(0, 202)
+    mySprite.lifespan = 10000
+}
+info.onLifeZero(function () {
+    game.over(false, color.Brighten)
+})
+function Map_maker () {
+    tiles.setTilemap(tilemap`level`)
+    for (let index = 0; index < 100; index++) {
+        tiles.setTileAt(tiles.getTileLocation(randint(0, 44), randint(0, 14)), myTiles.tile16)
+    }
+    for (let value of tiles.getTilesByType(myTiles.tile16)) {
+        tiles.setWallAt(value, true)
+    }
+    tiles.placeOnRandomTile(mySprite, myTiles.transparency16)
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.BasicEnemy, function (sprite, otherSprite) {
     scene.cameraShake(4, 500)
     timer.background(function () {
@@ -360,7 +425,7 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             statusbar.max = 100
             info.setLife(3)
             statusbar.positionDirection(CollisionDirection.Top)
-            Ammo = 15
+            Ammo = 20
             textSprite2 = textsprite.create("" + Ammo + " / 20")
             textSprite2.setFlag(SpriteFlag.RelativeToCamera, true)
             mySprite = sprites.create(img`
@@ -381,7 +446,6 @@ blockMenu.onMenuOptionSelected(function (option, index) {
                 . . . 5 f f f f f f f f 5 . . . 
                 . . . 5 5 5 5 5 5 5 5 5 5 . . . 
                 `, SpriteKind.Player)
-            tiles.placeOnRandomTile(mySprite, myTiles.transparency16)
             Camera = sprites.create(img`
                 . . . . . . . . . . . . . . . . 
                 . . . . . . . . . . . . . . . . 
@@ -402,40 +466,18 @@ blockMenu.onMenuOptionSelected(function (option, index) {
                 `, SpriteKind.Player)
             Camera.follow(mySprite, 80)
             scene.cameraFollowSprite(Camera)
-            tiles.setTilemap(tiles.createTilemap(hex`2d000f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                .............................................
-                `, [myTiles.transparency16], TileScale.Sixteen))
-            for (let index2 = 0; index2 < 100; index2++) {
-                tiles.setTileAt(tiles.getTileLocation(randint(0, 44), randint(0, 14)), myTiles.tile16)
-            }
-            for (let value of tiles.getTilesByType(myTiles.tile16)) {
-                tiles.setWallAt(value, true)
-            }
             mySprite.ay = 250
             controller.moveSprite(mySprite, 100, 0)
             color.startFade(color.White, color.originalPalette, 1000)
+            Map_maker()
             gameStarted = true
         })
     }
 })
-let Basic_Enemy: Sprite = null
-let Reward_Crate: Sprite = null
 let Camera: Sprite = null
 let textSprite2: Sprite = null
+let Basic_Enemy: Sprite = null
+let Reward_Crate: Sprite = null
 let projectile: Sprite = null
 let Direction = ""
 let statusbar: StatusBarSprite = null
@@ -452,58 +494,36 @@ textSprite.setMaxFontHeight(20)
 blockMenu.setColors(1, 15)
 game.onUpdate(function () {
     if (gameStarted) {
+        if (statusbar.value == 0) {
+            color.FadeToWhite.startScreenEffect(1000)
+            info.changeLifeBy(-1)
+            statusbar.value = statusbar.max
+            tiles.destroySpritesOfKind(SpriteKind.BasicEnemy)
+            tiles.destroySpritesOfKind(SpriteKind.Crate)
+            Crate_maker()
+            Enemy_Maker()
+            Map_maker()
+            timer.after(1500, function () {
+                color.startFade(color.White, color.originalPalette)
+            })
+        }
+    }
+})
+game.onUpdate(function () {
+    if (gameStarted) {
         textSprite2.destroy()
-        textSprite2 = textsprite.create("" + Ammo + " / 15", 15, 1)
+        textSprite2 = textsprite.create("" + Ammo + " / 20", 8, 1)
         textSprite2.setFlag(SpriteFlag.RelativeToCamera, true)
         textSprite2.setPosition(25, 20)
     }
 })
 game.onUpdateInterval(2000, function () {
     if (gameStarted) {
-        Reward_Crate = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . e e e e e e e e . . . . 
-            . . . . e f f f f f f e . . . . 
-            . . . . e f f e e f f e . . . . 
-            . . . . e e e e e e e e . . . . 
-            . . . . e f f e e f f e . . . . 
-            . . . . e f f f f f f e . . . . 
-            . . . . e f f f f f f e . . . . 
-            . . . . e e e e e e e e . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Crate)
-        tiles.placeOnTile(Reward_Crate, tiles.getTileLocation(randint(0, 44), 0))
-        Reward_Crate.setVelocity(0, 90)
+        //Crate_maker()
     }
 })
 game.onUpdateInterval(2000, function () {
     if (gameStarted) {
-        Basic_Enemy = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 f f f f f f f f 2 . . . 
-            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
-            `, SpriteKind.BasicEnemy)
-        tiles.placeOnTile(Basic_Enemy, tiles.getTileLocation(randint(0, 44), 0))
-        Basic_Enemy.setVelocity(0, 202)
-        mySprite.lifespan = 10000
+        //Enemy_Maker()
     }
 })
